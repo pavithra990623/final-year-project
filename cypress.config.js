2a3e27a8b1bce9bc4516a005db2e6cc320f7b13d
pavithra990623 <152731519+pavithra990623@users.noreply.github.com>
2024-03-24 15:11:22 +0530

    testing
    components

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,10 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  "e2e": {
+    "baseUrl": "http://localhost:3000" // Adjust the URL as needed
+  },
+
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
