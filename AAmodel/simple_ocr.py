576e1fe59a79682d51e62562f80db446d2ab4dd6
pavithra990623 <152731519+pavithra990623@users.noreply.github.com>
2024-04-10 20:32:45 +0530

    model
    ok

--- AAmodel/simple_ocr.py
+++ AAmodel/simple_ocr.py
@@ -3,6 +3,13 @@ import pytesseract
 import cv2
 import numpy as np
 import os
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+# Initialize Firebase Admin SDK
+cred = credentials.Certificate(r"D:\year3\A-individual project\final-year-project\login-c057b-firebase-adminsdk-yiql7-bb06c12b37.json")
+firebase_admin.initialize_app(cred)
+db = firestore.client()
 
 # Update the path below to where Tesseract is installed on your system
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
@@ -18,7 +25,6 @@ for filename in os.listdir(image_dir):
 
         # Convert PIL Image to a format OpenCV can work with
         image_cv = np.array(image)
-        
 
         # Preprocess the image
         # Convert to grayscale
@@ -35,3 +41,9 @@ for filename in os.listdir(image_dir):
 
         print(f"OCR result for {filename}:\n{text}\n")
         print("------------------------------------------------------\n")
+
+        # Send OCR result to Firebase
+        doc_ref = db.collection(u'ocr_results').document(filename)
+        doc_ref.set({
+            u'text': text
+        })
