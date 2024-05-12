f0b7c17a75944caa3bd7364f2a8f4c734c30ee1e
pavithra990623 <152731519+pavithra990623@users.noreply.github.com>
2024-05-12 20:30:32 +0530

    done
    ok

---
+++ frontend/src/Pages/DocPro.js
@@ -0,0 +1,64 @@
+// import React from 'react';
+// import { navigate } from 'react-router-dom'; // Assuming you're using React Router
+// import { BsFillGrid3X3GapFill } from 'react-icons/bs'; // Assuming you're using React Icons
+
+// function DocPro() {
+
+//     const handleNavigateToImageUpload = () => {
+//     navigate('/imageupload');
+//   };
+
+//   const handleNavigateToDetails = () => {
+//     navigate('/details');
+//   };
+
+//   const handleNavigateToManualmes = () => {
+//     navigate('/manualmes');
+//   };
+
+//   const handleNavigateToLab = () => {
+//     navigate('/lab');
+//   };
+
+//   return (
+//     <div>
+//       <div className='cardprofile'>
+//         <div className='card-innerprofile'>
+//           <h3>UPDATE</h3>
+//           <BsFillGrid3X3GapFill className='card_icon' />
+//         </div>
+//         <h1>01</h1>
+//         <button9 type="button" onClick={handleNavigateToDetails}>Profile</button9>
+//       </div>
+
+//       <div className='cardprofile3'>
+//         <div className='card-innerprofile3'>
+//           <h3>VIEW REPORT</h3>
+//           <BsFillGrid3X3GapFill className='card_icon' />
+//         </div>
+//         <h1>03</h1>
+//         <button9 type="button" onClick={handleNavigateToImageUpload}>Lab Result</button9>
+//       </div>
+
+//       <div className='cardprofile4'>
+//         <div className='card-innerprofile4'>
+//           <h3>MANUAL</h3>
+//           <BsFillGrid3X3GapFill className='card_icon' />
+//         </div>
+//         <h1>04</h1>
+//         <button9 type="button" onClick={handleNavigateToManualmes}>Mannual Mesurement</button9>
+//       </div>
+
+//       <div className='cardprofile5'>
+//         <div className='card-innerprofile5'>
+//           <h3>LAB</h3>
+//           <BsFillGrid3X3GapFill className='card_icon' />
+//         </div>
+//         <h1>05</h1>
+//         <button9 type="button" onClick={handleNavigateToLab}>Lab</button9>
+//       </div>
+//     </div>
+//   )
+// }
+
+// export default DocPro;
